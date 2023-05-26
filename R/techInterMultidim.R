4fe31d20675c6673398fb9c79ebccf163d7e9d5a
Marc Taylor <marchtaylor@gmail.com>
2023-05-26 15:59:19 +0200

    tech interaction non-parametric

--- R/techInterMultidim.R
+++ R/techInterMultidim.R
@@ -1,39 +1,38 @@
-library(pals)
-library(reshape2)
 
+# required libraries ------------------------------------------------------
+
+library(pals)
+library(data.table)
+library(vegan)
 
 # required data -----------------------------------------------------------
 
 load("shiny/Data/fdi_ecoregion/Greater North Sea_data.Rdata")
 
-# aggregate data and cast species dimension as columns -----------------------------------------------------
+# aggregate data using data.table functions ----------------------------------
+data <- as.data.table(data)
 
-agg <- aggregate(
-  totwghtlandg ~ icesname + gear_type + year + quarter + species, 
-  data = data, FUN = sum, na.rm = T)
-names(agg)[6] <- "landings"
+agg <- data[, .(landings = sum(totwghtlandg, na.rm = TRUE)), 
+            by = .(icesname, gear_type, year, quarter, species)]
+agg[, landingsSum := sum(landings), by = .(year, species)]
+agg[, landingsPerc := landings/landingsSum]
 
-dat <- reshape2::dcast(data = agg, 
-  formula = icesname + gear_type + year + quarter ~ species, 
-  value.var = "landings", fill = 0)
+# reshape data using dcast from data.table ----------------------------------
+dat <- dcast(agg, icesname + gear_type + year + quarter ~ species, 
+             value.var = "landingsPerc", fill = 0)
 
-# calculate distance matrix among species-----------------------------------------------
+# calculate distance matrix among species using base R dist ---------------
 
-M <- as.matrix(dat[,-c(1:4)])
-D <- cor(log(M+1)) # Pearson correlation of log landings
+M <- as.matrix(dat[, -c(1:4)])
+# D <- cor(log(M+1)) # Pearson correlation of log landings
 # D <- cor(M) # Pearson correlation of log landings
+D <- vegdist(t(M), method = "bray")
 
-# plot distance matrix as heatmap ------------------------------------------------------------
+# plot distance matrix as heatmap -------------------------------------------
 
-COL <- c("#181C43", "#232D6B", "#273D98", "#1554BA", "#166FBB", "#3787BA", 
-  "#599BBA", "#81AFC0", "#A8C1CB", "#CDD6D9", "#F0ECEB", "#E5CEC7", 
-  "#DBB0A3", "#D2937E", "#C8765A", "#BF583B", "#B23727", "#9C1826", 
-  "#7E0E28", "#5C0D20", "#3C0912")
+COL <- pals::brewer.spectral(100)
 
-heatmap(D, col = COL, zlim = c(-1,1), symm =T, 
-  margins = c(3,2))
-
-
-
-
+png("output/techInterMultidim_ex.png", width = 5, height = 5, units = "in", res = 400)
+heatmap(as.matrix(D), col = COL, zlim = c(0, 1), symm = TRUE, margins = c(3, 4))
+dev.off()
 
